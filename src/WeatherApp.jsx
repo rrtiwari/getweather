d960ee4778284d43379e9f278ea2f7de96977e20
rrtiwari <128848653+rrtiwari@users.noreply.github.com>
2024-06-21 22:30:24 +0530

    fcommit

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,32 +1,3 @@
-// import { useState } from "react"
-// import InfoBox from "./InfoBox"
-// import SearchBox from "./SearchBox"
-
-
-
-// export default function WeatherApp() {
-//     let [weatherInfo, setWeatherInfo] = useState({
-//         city: "Kolkata",
-//         feelsLike: 19.22,
-//         humidity: 88,
-//         temp: 18.97,
-//         tempMax: 18.97,
-//         tempMin: 18.97,
-//         weather: "mist"
-//     });
-
-//     let updateInfo = (newInfo) => {
-//         setWeatherInfo(newInfo);
-//     }
-//     return (
-//         <div style={{textAlign: "center"}}>
-//             <h2 >Weather App by Saikat</h2>
-//             <SearchBox updateInfo={updateInfo}/>
-//             <InfoBox info={weatherInfo}/>
-//         </div>
-//     )
-// }
-
 
 import { useState } from 'react';
 import InfoBox from './InfoBox';
